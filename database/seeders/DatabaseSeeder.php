bc4c32905e6f2299a404af2da528c302e4b2f220
ammar <codephp001@gmail.com>
2022-08-21 16:55:04 +0300

    add seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            TeamSeeder::class,
+            //TeamSeeder::class,
+            LeagueSeeder::class,
+            GameSeeder::class
         ]);
     }
 }
--- database/seeders/GameSeeder.php
+++ database/seeders/GameSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Game;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,33 @@ class GameSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $game=new Game();
+        $game->league_id=1;
+        $game->team_id_1=1;
+        $game->team_id_2=2;
+        $game->date_time=Carbon::createFromFormat('d/m/Y',  '05/08/2022');;
+        $game->save();
+
+        $game=new Game();
+        $game->league_id=1;
+        $game->team_id_1=3;
+        $game->team_id_2=4;
+        $game->date_time=Carbon::createFromFormat('d/m/Y',  '15/08/2022');;
+        $game->save();
+
+        $game=new Game();
+        $game->league_id=1;
+        $game->team_id_1=2;
+        $game->team_id_2=3;
+        $game->date_time=Carbon::createFromFormat('d/m/Y',  '25/08/2022');;
+        $game->save();
+
+        $game=new Game();
+        $game->league_id=1;
+        $game->team_id_1=1;
+        $game->team_id_2=4;
+        $game->date_time=Carbon::createFromFormat('d/m/Y',  '05/09/2022');;
+        $game->save();
     }
 }
+
