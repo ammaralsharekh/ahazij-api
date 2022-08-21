1ef1bed565d975d6d01138e27d3c1d485dfde369
ammar <codephp001@gmail.com>
2022-08-21 13:07:33 +0300

    add team rout index

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
+Route::group([ ], function () {
+    Route::get('team', 'Api\TeamController@index');
+});
 
 Route::group([
     'prefix' => 'auth'
