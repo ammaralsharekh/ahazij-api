291f658b29615358dc8b32dc3c23180a7faa647d
ammar <codephp001@gmail.com>
2022-08-26 10:13:10 +0300

    add token resource

--- app/Http/Resources/TokenResource.php
+++ app/Http/Resources/TokenResource.php
@@ -2,27 +2,31 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class GameCollection extends ResourceCollection
+class TokenResource extends JsonResource
 {
     /**
      * The "data" wrapper that should be applied.
      *
      * @var string|null
      */
-    public static $wrap = 'games';
+    public static $wrap = 'token';
     /**
      * Transform the resource collection into an array.
      *
     /**
-     * Transform the resource collection into an array.
+     * Transform the resource into an array.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'token'=>$this->accessToken->token,
+            'expires_at'=>$this->accessToken->expires_at,
+            'tokenable_id'=>$this->accessToken->tokenable_id
+        ];
     }
 }
